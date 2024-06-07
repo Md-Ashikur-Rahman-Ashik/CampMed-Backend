0b894bd25d0baf3aa7e8c1f71d6cca5f4882452f
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-07 21:16:42 +0600

    Updated route for participant

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ async function run() {
       res.send(result);
     });
 
-    app.patch("/participant/:id", verifyToken, async (req, res) => {
+    app.patch("/participants/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateStatus = {
